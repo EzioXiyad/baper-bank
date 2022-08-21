20914e82425b6965bbd0068659770f08782fe3d2
EzioXiyad <zakariamasud466@gmail.com>
2022-08-21 19:12:15 +0600

    completed withdraw and balance field

--- js/deposite.js
+++ js/deposite.js
@@ -9,5 +9,35 @@ document.getElementById('btn-deposite').addEventListener('click',function(){
     const currentDeposite=previousDeposite+newDeposite;
     depositeElement.innerText=currentDeposite;
 
+    const previousBalance=document.getElementById('balance-total');
+    const balanceString=previousBalance.innerText;
+
+    const balance=parseFloat(balanceString);
+
+    const totalBalance=newDeposite+balance;
+    previousBalance.innerText=totalBalance;
+
     deposite.value='';
+})
+
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    const withdraw=document.getElementById('user-withdraw');
+    const newWithdrawString= withdraw.value;
+    const newWithdraw=parseFloat(newWithdrawString);
+
+    const withdrawElement=document.getElementById('withdraw-total');
+    const previousWithdrawString=withdrawElement.innerText;
+    const previousWithdraw=parseFloat(previousWithdrawString);
+    
+    const totalWithdraw= previousWithdraw+newWithdraw;
+    withdrawElement.innerText=totalWithdraw;
+
+    const previousBalance=document.getElementById('balance-total');
+    const previousBalanceString=previousBalance.innerText;
+    const Balance=parseFloat(previousBalanceString);
+
+    const totalBalance=Balance-newWithdraw;
+    previousBalance.innerText=totalBalance;
+
+    withdraw.value='';
 })
